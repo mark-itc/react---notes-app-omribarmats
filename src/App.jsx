77250be42758c3c4c9cf34536d830298335968cf
omribarmats <omri.bar@gmail.com>
2022-11-10 12:24:20 +0200

    Milestone-3 Done
    1. Added a title to notes above the text

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,9 @@ import { useState } from 'react'
 function App() {
   const [notes, setNotes] = useState([])
 
-  const handleAddNotes = (noteText) => {
+  const handleAddNotes = (noteText, noteTitle) => {
     const note = {
+      title: noteTitle,
       text: noteText,
       date: new Date(),
       id: notes.length === 0 ? 1 : notes[notes.length - 1].id + 1,
@@ -16,8 +17,8 @@ function App() {
   }
 
   const handleDelteNote = (id) => {
-    if (global.confirm("Are you sure you want to delete note " + id + " ?")) {
-      window.open(deleteNote(id), "exit.html");
+    if (window.confirm("Are you sure you want to delete note " + id + " ?") == true) {
+      deleteNote(id)
     }
   }
 
@@ -30,13 +31,13 @@ function App() {
       <form className="form">
         <button onClick={(e) => {
           e.preventDefault();
-          handleAddNotes('Example note')
+          handleAddNotes('Example note', 'Note title')
         }}
         >Add Note</button>
       </form>
       <div className="Notes-list">
         {notes.map((note, index) => {
-          return <Note key={index} date={note.date} text={note.text} id={note.id} handleDelteNote={handleDelteNote} />
+          return <Note key={index} date={note.date} text={note.text} id={note.id} title={note.title} handleDelteNote={handleDelteNote} />
         })}
       </div>
     </div>
--- src/Components/Note.jsx
+++ src/Components/Note.jsx
@@ -3,7 +3,8 @@ import './Notes.css'
 export const Note = (props) => {
 
     return <div className="note">
-        <h2>{props.id}. {props.text}</h2>
+        <h2>{props.id}. {props.title}</h2>
+        <p>{props.text}</p>
         <p>{props.date.toDateString()}</p>
         <button onClick={(e) => {
             e.preventDefault();
